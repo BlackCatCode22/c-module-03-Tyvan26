7dd1b596df434e40a47fd055456ece8d1c8ae70c
Tyvan26 <1024772@MY.SCCCD.EDU>
2025-02-22 19:52:10 -0800

    Add files via upload

---
+++ linkedList.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void deleteByValue(int val) {
+        if (!head) return;
+        if (head->data == val) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next && temp->next->data != val) {
+            temp = temp->next;
+        }
+        if (temp->next) {
+            Node* toDelete = temp->next;
+            temp->next = temp->next->next;
+            delete toDelete;
+        }
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout << "NULL" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+
+    std::cout << "Linked List: ";
+    list.display();
+
+    list.deleteByValue(20);
+    std::cout << "After deleting 20: ";
+    list.display();
+
+    return 0;
+}
